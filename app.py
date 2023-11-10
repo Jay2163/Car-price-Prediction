492c7fd380e440b66887d8a509652e5770cb4c27
Jay Butani <jaybutani606@gmail.com>
2023-11-10 22:46:09 +0530

    third commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pickle
-from flask import Flask,request,app,jsonify,url_for,render_template
+from flask import Flask,request,app,jsonify,render_template,url_for
 import numpy as np
 import pandas as pd
 
@@ -20,6 +20,15 @@ def predict_api():
     output = model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [x for x in request.form.values()]
+    final_input = np.array(data).reshape(1,-1)
+    print(final_input)
+    output = model.predict(final_input)
+    return render_template("home.html",prediction_text="The Car Price Prediction is {}".format(round(output[0],2)))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
